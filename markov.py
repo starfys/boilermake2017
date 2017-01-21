964c588736ee2f15236e6261c700d74901268323
sremedios <sam.remedios@gmail.com>
2017-01-21 03:37:15 -0600

    markov up yo

--- markov.py
+++ markov.py
@@ -4,6 +4,10 @@ from pymongo import MongoClient
 import re
 import random
 
+from base64 import b64encode, b64decode
+b64enc = lambda string: b64encode(string.encode('utf-8')).decode('utf-8')
+b64dec = lambda string: b64decode(string.encode('utf-8')).decode('utf-8')
+
 client = MongoClient('mongodb://localhost:27017')
 db = client['boilermake']
 collection = db['all_words']
@@ -13,7 +17,7 @@ max_words = 100
 
 def generate():
     generated_sentence = []
-    prev_word = 'START_TOKEN'
+    prev_word = b64enc('START_TOKEN')
     for _ in range(max_words):
         choice = random.randint(0, s.find_one()[prev_word]['SUM'])
         
@@ -24,10 +28,11 @@ def generate():
             choice -= freq
             if choice <= 0:
                 break
-        if new_word == 'END_TOKEN':
+        if new_word == b64enc('END_TOKEN'):
             break
+        new_word = b64dec(new_word)
         generated_sentence.append(new_word)
-        prev_word = new_word
+        prev_word = b64enc(new_word)
     return ' '.join(generated_sentence)
 
 print(generate())
