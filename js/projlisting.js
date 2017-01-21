ec0e532b345135c1b3475821b331e1df8622e5e8
Taylor Harvin <tharvin12@gmail.com>
2017-01-21 11:09:50 -0600

    Added chart tool. Added some further web interface improvements.  Included Markov Chain gen in js (from user input).

--- js/projlisting.js
+++ js/projlisting.js
@@ -1,15 +1,44 @@
 hackIdeasApp.controller("projListing",function($scope, $rootScope,$http){
   $scope.enteredHackIdea = "Enter Idea";
-  $scope.hackProjects = [];
+  $scope.hackProjects = [{"projName":"Hack Project 1","projDesc":"This is the description of project 1"},{"projName":"Hack Project 2","projDesc":"This is the description of project 2"}];
+  $scope.generatedIdea = "Select projects to generate an idea.";
+  $scope.selectedProjects = [];
+  $scope.checkedFlag = false;
+
+  $scope.refreshProjectList = function(){
+      $http({
+        method: "GET",
+        url: "<<DBLOC>>"
+      }).then(function success(response){
+        //console.log(response.data);
+        //var myArray = JSON.parse(response.data);
+        $scope.hackProjects = response.data;
+      },function error(response){
+        alert(response.statusText);
+      });
+    };
 
 
-
-
-
-
-  $scope.$on("requestProjectList", function(event, args){
+  /*$scope.$on("requestProjectList", function(event, args){
     $scope.hackProjects = args.projectList;
-  });
+  });*/
 
+  $scope.toggleSelectedProject = function(projName){
+    console.log(includeProj);
+    var projIndex = $scope.selectedProjects.indexOf(projName);
+    var includeProj = $("#"+projName+"_check").is(':checked');
+    alert(includeProj);
+    console.log($("#"+projName+"_check"));
+    if(includeProj){
+      if(projIndex <= -1){
+        $scope.selectedProjects.push(projName);
+      }
+    }
+    else{
+      if(projIndex >= 0){
+        $scope.selectedProjects.splice();
+      }
+    }
+  }
 
 });
--- js/searchengine.js
+++ js/searchengine.js
@@ -1,20 +1,62 @@
 var hackIdeasApp = angular.module('hackIdeasApp',[]);
 
-
-
-
-
 hackIdeasApp.controller("searchEngine",function($scope, $rootScope,$http){
-  $scope.enteredHackIdea = "test";
+  $scope.enteredHackIdea = "This is a test sentence, and this test is a great test.";
   $scope.technologiesList = ["C/C++","PHP","Machine Learning","Javascript"];
-  $scope.hackProjects = [{"projName":"Hack Project 1","projDesc":"This is the description of project 1"},{"projName":"Hack Project 2","projDesc":"This is the description of project 2"}];
+
+  /*$scope.markovChain_test = [{"this":{"is":0.5,"test":0.5}},
+                        {"is":{"a":1.0}},
+                        {"test":{"is":0.5,"sentence":1.0}},
+                        {"a":{"great":0.5,"test":0.5}},
+                        {"great":{"test":1.0}},
+                        {"sentence":{"and":1.0}},
+                        {"and":{"this":1.0}},
+    ];*/
+
+  $scope.userMarkovChain = [];
+
+  $scope.genMarkovChain = function(enteredHackIdea_cp){
+    var modString = enteredHackIdea_cp.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"").toLowerCase();
+    var splitStr = modString.split(" ");
+    var currWord = "";
+    var nextWord = "";
+
+    //console.log(splitStr);
+
+    $scope.userMarkovChain = new Object();
+
+    for(wordIndex in splitStr){
+      currWord = splitStr[wordIndex];
+      if(wordIndex < splitStr.length){
+        nextWord = splitStr[parseInt(wordIndex,10) + 1];
+        //console.log(currWord +" , "+nextWord);
+        if(typeof $scope.userMarkovChain[currWord] !== 'undefined'){
+          if(typeof $scope.userMarkovChain[currWord][nextWord] !== 'undefined'){
+            $scope.userMarkovChain[currWord][nextWord]++;
+            //console.log("NEXT: "+nextWord);
+            //console.log($scope.userMarkovChain[currWord][nextWord]);
+          }
+          else{
+            $scope.userMarkovChain[currWord][nextWord] = 1;
+          }
+        }
+        else{
+          $scope.userMarkovChain[currWord] = new Object();
+          $scope.userMarkovChain[currWord][nextWord] = 1;
+        }
+      }
+    }
+
+    console.log($scope.userMarkovChain);
+  }
 
 
   $scope.submitIdea = function(){
-    $rootScope.$broadcast("requestProjectList",
+    /*$rootScope.$broadcast("requestProjectList",
       {
         projectList:$scope.hackProjects
-      });
+      });*/
+    $scope.genMarkovChain($scope.enteredHackIdea);
   }
 
 
